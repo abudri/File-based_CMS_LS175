b9af93634db620e35408002d0176fd44cba3ff92
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-07-26 08:27:14 -0400

    adding sessions to project, as well as catching non-existent urls and redirecting

--- cms.rb
+++ cms.rb
@@ -4,6 +4,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+configure do
+  enable :sessions # tells sinatra to activate it's session support
+  set :sessions_secret, 'super secret' # setting the session secret, to the string 'secret'
+end
+
+configure do
+  set :erb, escape_html: true # Lesson 6, Sanitizing HTML: https://launchschool.com/lessons/31df6daa/assignments/d98e4174
+end
+
 root = File.expand_path(__dir__)
 
 get '/' do
@@ -13,6 +22,12 @@ end
 
 get '/:filename' do
   file_path = root + '/data/' + params[:filename]
-  headers['Content-Type'] = 'text/plain'
-  File.read(file_path) # this simply displays contexts of file, such as /data/history.txt.  We refefence the file in the applicaiton from root or home though, /history.txt
+
+  if File.file?(file_path)
+    headers['Content-Type'] = 'text/plain'
+    File.read(file_path) # this simply displays contexts of file, such as /data/history.txt.  We refefence the file in the applicaiton from root or home though, /history.txt
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect '/'
+  end
 end
